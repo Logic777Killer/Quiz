a4df7c31000409a78ea9ee2068fa394ff7f56cfd
Logic777Killer <ilyakopiy@gmail.com>
2026-05-04 20:37:39 +0300

    Добавлена кнопка профиля и страница профиля

--- surveys/views.py
+++ surveys/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'surveys/index.html')
+
+def profile(request):
+    return render(request, 'surveys/profile.html')
+
