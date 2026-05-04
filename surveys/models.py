c7c570fdb88a430a7ea52859c5303386f621e4c5
Logic777Killer <ilyakopiy@gmail.com>
2026-05-04 20:47:26 +0300

    Добавлены поля регистрации

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Модели добавим позже
+
+class Profile(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Мужской'),
+        ('female', 'Женский'),
+        ('other', 'Другое'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    country = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
--- surveys/views.py
+++ surveys/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 def index(request):
     return render(request, 'surveys/index.html')
 
+
 def profile(request):
     return render(request, 'surveys/profile.html')
 
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        gender = request.POST.get("gender")
+        country = request.POST.get("country")
+        age = request.POST.get("age")
+
+        if password1 != password2:
+            return render(request, "surveys/register.html", {"error": "Пароли не совпадают"})
+
+        if User.objects.filter(username=username).exists():
+            return render(request, "surveys/register.html", {"error": "Такой логин уже существует"})
+
+        user = User.objects.create_user(username=username, password=password1)
+
+        Profile.objects.create(
+            user=user,
+            gender=gender,
+            country=country,
+            age=age
+        )
+
+        return redirect("/")
+
+    return render(request, "surveys/register.html")
