a84c80f35c70351680530952eb1ad60a2ffbb24e
Logic777Killer <ilyakopiy@gmail.com>
2026-05-06 15:29:45 +0300

    добавлены модели

--- surveys/models.py
+++ surveys/models.py
@@ -17,3 +17,43 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Профиль {self.user.username}"
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Survey(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
